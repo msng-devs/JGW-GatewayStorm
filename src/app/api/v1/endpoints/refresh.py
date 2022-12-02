a9d3adb0774c4bd1171ffa6ed7436f8392e556e4
Junser Hwang <hzser123@gmail.com>
2022-12-03 01:03:08 +0900

    [FIX] prefix 오타 수정

--- src/app/api/v1/endpoints/refresh.py
+++ src/app/api/v1/endpoints/refresh.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Header, HTTPException, Request
 from app.configs.config import settings
 from app.utlis.logger import logger
 
-refresh_router = APIRouter(prefix="gs/api/v1/refresh",tags=["refresh"])
+refresh_router = APIRouter(prefix="/gs/api/v1/refresh",tags=["refresh"])
 
 @refresh_router.get("")
 async def gateway_refresh(user_pk: Union[str, None] = Header(default=None),role_pk: Union[int, None] = Header(default=None)):
